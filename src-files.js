268eda583eea0ec4da66decb1dbc8aa2fcd3e152
equation314 <equation618@gmail.com>
2024-07-17 07:44:30 +0000

    Deploying to gh-pages from @ arceos-org/kspin@f68249a86c0adf6a8a11575207c31b82b6e151a0 🚀

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["kspin",["",[],["base.rs","lib.rs"]]]\
+]'));
+createSrcSidebar();
---
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"kspin":[["impl&lt;'a, G: BaseGuard, T: ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"kspin/struct.BaseSpinLockGuard.html\" title=\"struct kspin::BaseSpinLockGuard\">BaseSpinLockGuard</a>&lt;'a, G, T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
